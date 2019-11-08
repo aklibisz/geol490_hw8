2c00a414bdd180cf014203122939bbb99ff0a5db
Adam Klibisz <aklibis1@vols.utk.edu>
2019-11-08 09:50:33 -0500

    Fixed some syntax and added action button

--- hw_8/app.R
+++ hw_8/app.R
@@ -2,8 +2,13 @@
 # Adam Klibisz Geol 490 HW8
 ####
 
+# some stuff to run before anything else
 library(shiny)
 library(tidyverse)
+
+# limit the range of selectable MPG to the actual range of MPG
+min.mpg <- min(mtcars$mpg)
+max.mpg <- max(mtcars$mpg)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -14,11 +19,15 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("mpgRange",
+                        "Range of MPG",
+                        min = min.mpg,
+                        max = max.mpg,
+                        value = c(min.mpg, max.mpg)),
+    
+        actionButton("go", 
+                     "Go!",
+                     icon = icon("thumbs-up"))
         ),
 
         # Show a plot of the generated distribution
